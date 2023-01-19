2833d38b07aca95bc60cde3d46a7023a5dbb12a9
Prabhav Nadipi Reddy <prabhav.nr@gmail.com>
2023-01-19 20:30:27 +0000

    Made new javascript files and added questions to questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,52 @@
+let questions = [
+    {
+        "title": "Which of the following is not javascript data types?",
+        "choices": ["Null type", "Undefined type", "Number type", "All of the above"],
+        "answers": "All of the above"
+    },
+    {
+        "title": "Which of the following can be used to call a JavaScript Code Snippet?",
+        "choices": ["Function/Method", "Preprocessor", "Triggering Event", "RMI"],
+        "answers": "Function/Method"
+    },
+    {
+        "title": "Which of the following explains correctly what happens when a JavaScript program is developed on a Unix Machine?",
+        "choices": ["will work perfectly well on a Windows Machine","will be displayed as JavaScript text on the browser","will throw errors and exceptions","must be restricted to a Unix Machine only"],
+        "answers": "will work perfectly well on a Windows Machine"
+    },
+    {
+        "title": "Javascript is an __________ language",
+        "choices": ["Object-oriented", "Object-based", "Procedural", "None of the above"],
+        "answers": "Object-oriented"
+    },
+    {
+        "title": "Which of the following keywords is used to define a variable in Javascript?",
+        "choices": ["var", "let", "Both A and B", "None of the above"],
+        "answers": "Both A and B"
+    },
+    {
+        "title": "Which of the following methods is used to access HTML elements using Javascript",
+        "choices": ["getElementbyId()", "getElementbyClassName", "Both A and B", "None of the above"],
+        "answers": "Both A and B"
+    },
+    {
+        "title": "Which of the following methods can be used to display data in some form using Javascript?",
+        "choices": ["document.write()", "console.log()", "window.alert()", "All of the above"],
+        "answers": "All of the above"
+    },
+    {
+        "title": "When the switch statement matches the expression with the given labels, how is the comparison done?",
+        "choices": ["Both the datatype and the result of the expression are compared","Only the datatype of the expression is compared","Only the value of the expression is compared","None of the above"],
+        "answers": "Both the datatype and the result of the expression are compared"
+    },
+    {
+        "title": "What keyword is used to check whether a given property is valid or not?",
+        "choices": ["in", "is in", "exists", "lies"],
+        "answers": "in"
+    },
+    {
+        "title": "When an operator’s value is NULL, the typeof returned by the unary operator is:",
+        "choices": ["Boolean", "Undefined", "Object", "Integer"],
+        "answers": "Object"
+    },
+]
